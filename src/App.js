05c272ab215f967cfce30b1226f75be60e5a1ae4
Atharv <atharv.n2002@gmail.com>
2022-12-10 23:17:49 +0530

    Intital files added and cart-context added

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCardHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCardHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    //  <React.Fragment>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCardHandler} />}
+      <Header onShowCart={showCardHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+
+    //  </React.Fragment>
+  );
+}
+
+export default App;
